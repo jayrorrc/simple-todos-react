49f598dc55b07e09440ffe5d3e0ba4fa09353101
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2023-04-12 15:04:06 -0300

    feature: add user

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,23 +1,41 @@
-import React, { useState } from 'react';
+import { Meteor } from 'meteor/meteor';
+import React, { useState, Fragment } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Task } from './Task';
 import { TasksCollection } from '/imports/api/TasksCollection';
+import { Task } from './Task';
 import { TaskForm } from './TaskForm';
+import { LoginForm } from './LoginForm';
 
 export const App = () => {
+  const user = useTracker(() => Meteor.user());
   const [ hideCompleted, setHideCompleted ] = useState(false);
 
   const hideCompletedFilter = { isChecked: { $ne: true } };
-  const tasks = useTracker(
-    () => TasksCollection.find(
-      hideCompleted ? hideCompletedFilter : {},
-      { sort: { createdAt: -1 } }
-    ).fetch()
-  );
 
-  const pendingTasksCount = useTracker(() =>
-    TasksCollection.find(hideCompletedFilter).count()
-  );
+  const userFilter = user ? { userId: user._id } : {};
+
+  const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter };
+
+  const tasks = useTracker(() => {
+    if (!user) {
+      return [];
+    }
+
+    return TasksCollection.find(
+      hideCompleted ? pendingOnlyFilter : userFilter,
+      {
+        sort: { createdAt: -1 },
+      }
+    ).fetch();
+  });
+
+  const pendingTasksCount = useTracker(() => {
+    if (!user) {
+      return 0;
+    }
+
+    return TasksCollection.find(pendingOnlyFilter).count();
+  });
 
   const pendingTasksTitle = `${
     pendingTasksCount ? ` (${pendingTasksCount})` : ''
@@ -33,35 +51,51 @@ export const App = () => {
 
   const deleteTask = ({ _id }) => TasksCollection.remove(_id);
 
+  const logout = () => Meteor.logout();
+
   return (
     <div className="app">
       <header>
         <div className="app-bar">
           <div className="app-header">
-            <h1>To Do List {pendingTasksTitle}</h1>
+            <h1>
+              📝️ To Do List {pendingTasksTitle}
+            </h1>
           </div>
         </div>
       </header>
 
       <div className="main">
-        <TaskForm />
+        {
+          user ? (
+            <Fragment>
+              <div className="user" onClick={logout}>
+                {user.username} 🚪
+              </div>
 
-        <div className="filter">
-         <button onClick={() => setHideCompleted(!hideCompleted)}>
-           {hideCompleted ? 'Show All' : 'Hide Completed'}
-         </button>
-       </div>
+              <TaskForm user={user} />
 
-        <ul className="tasks">
-          {tasks.map(task => (
-            <Task
-              key={task._id}
-              task={task}
-              onCheckboxClick={toggleChecked}
-              onDeleteClick={deleteTask}
-            />
-          ))}
-        </ul>
+              <div className="filter">
+                <button onClick={() => setHideCompleted(!hideCompleted)}>
+                  {hideCompleted ? 'Show All' : 'Hide Completed'}
+                </button>
+              </div>
+
+              <ul className="tasks">
+                {tasks.map(task => (
+                  <Task
+                    key={task._id}
+                    task={task}
+                    onCheckboxClick={toggleChecked}
+                    onDeleteClick={deleteTask}
+                  />
+                ))}
+              </ul>
+            </Fragment>
+          ) : (
+            <LoginForm />
+          )
+        }
       </div>
     </div>
   );
